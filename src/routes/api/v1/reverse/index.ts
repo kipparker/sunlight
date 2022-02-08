9b3104e9ab34e9acc1060d6bce4435193f86936a
Kip Parker <kip@kipparker.co.uk>
2022-02-08 15:34:37 +0000

    Updates API interface to new Svelte API

--- src/routes/api/v1/reverse/index.ts
+++ src/routes/api/v1/reverse/index.ts
@@ -4,8 +4,8 @@ const API_URL = 'https://api.opencagedata.com/geocode/v1/json'
 
 
 export const get: RequestHandler = async (request) => {
-    console.log(request.query['latitude'])
-    const request_url = `${API_URL}?key=${import.meta.env.VITE_OPENCAGE_APIKEY}&q=${encodeURIComponent(request.query.get('latitude') + ',' + request.query.get('longitude'))}&pretty=1&no_annotations=1`
+    console.log(request.url.searchParams['latitude'])
+    const request_url = `${API_URL}?key=${import.meta.env.VITE_OPENCAGE_APIKEY}&q=${encodeURIComponent(request.url.searchParams.get('latitude') + ',' + request.url.searchParams.get('longitude'))}&pretty=1&no_annotations=1`
     console.log(request_url)
     const response = await fetch(request_url, {
         method: 'GET',
